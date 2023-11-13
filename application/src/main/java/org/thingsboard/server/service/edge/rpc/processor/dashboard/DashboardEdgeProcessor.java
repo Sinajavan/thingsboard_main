thingsboard/thingsboard

Refactoring after final review

application/src/main/java/org/thingsboard/server/service/edge/rpc/processor/dashboard/DashboardEdgeProcessor.java
@@ -81,7 +81,7 @@ public class DashboardEdgeProcessor extends BaseDashboardProcessor {
 
     private void saveOrUpdateDashboard(TenantId tenantId, DashboardId dashboardId, DashboardUpdateMsg dashboardUpdateMsg, Edge edge, EdgeVersion edgeVersion) {
         boolean created = super.saveOrUpdateDashboard(tenantId, dashboardId, dashboardUpdateMsg,
-                EdgeVersionUtils.isEdgeProtoDeprecated(edgeVersion));
+                EdgeVersionUtils.isEdgeProtoDeprecated(edgeVersion), edge.getCustomerId());
         if (created) {
             createRelationFromEdge(tenantId, edge.getId(), dashboardId);
             pushDashboardCreatedEventToRuleEngine(tenantId, edge, dashboardId);

Sinajavan/thingsboard_main

application/src/main/java/org/thingsboard/server/service/edge/rpc/processor/dashboard/DashboardEdgeProcessor.java
@@ -80,7 +80,7 @@ public class DashboardEdgeProcessor extends BaseDashboardProcessor {
 
     private void saveOrUpdateDashboard(TenantId tenantId, DashboardId dashboardId, DashboardUpdateMsg dashboardUpdateMsg, Edge edge, EdgeVersion edgeVersion) {
         boolean created = super.saveOrUpdateDashboard(tenantId, dashboardId, dashboardUpdateMsg,
-                EdgeVersionUtils.isEdgeVersionOlderThan_3_6_2(edgeVersion));
+                EdgeVersionUtils.isEdgeVersionOlderThan_3_6_2(edgeVersion), edge.getCustomerId());
         if (created) {
             createRelationFromEdge(tenantId, edge.getId(), dashboardId);
             pushDashboardCreatedEventToRuleEngine(tenantId, edge, dashboardId);