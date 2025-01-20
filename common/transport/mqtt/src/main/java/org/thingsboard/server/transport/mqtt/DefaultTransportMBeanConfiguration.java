smatvienko-tb/thingsboard

DefaultTransportMBeanConfiguration

common/transport/transport-api/src/main/java/org/thingsboard/server/common/transport/service/DefaultTransportMBeanConfiguration.java
@@ -1,0 +1,48 @@
+/**
+ * Copyright © 2016-2024 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.common.transport.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.MBeanExporter;
+import org.thingsboard.server.queue.util.TbTransportComponent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@TbTransportComponent
+@RequiredArgsConstructor
+public class DefaultTransportMBeanConfiguration {
+
+    private final DefaultTransportService transportService;
+
+    @Bean
+    public HashMapObserver hashMapObserver() {
+        return new HashMapObserver(transportService.sessions);
+    }
+
+    @Bean
+    public MBeanExporter mBeanExporter() {
+        MBeanExporter exporter = new MBeanExporter();
+        Map<String, Object> beans = new HashMap<>();
+        beans.put("org.thingsboard:type=HashMapObserver", hashMapObserver());
+        exporter.setBeans(beans);
+        return exporter;
+    }
+
+}

Sinajavan/thingsboard_main

common/transport/mqtt/src/main/java/org/thingsboard/server/transport/mqtt/DefaultTransportMBeanConfiguration.java
@@ -1,0 +1,49 @@
+/**
+ * Copyright © 2016-2024 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.transport.mqtt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.MBeanExporter;
+import org.thingsboard.server.common.transport.service.DefaultTransportService;
+import org.thingsboard.server.queue.util.TbTransportComponent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@TbTransportComponent
+@RequiredArgsConstructor
+public class DefaultTransportMBeanConfiguration {
+
+    private final DefaultTransportService transportService;
+
+    @Bean
+    public HashMapObserver hashMapObserver() {
+        return new HashMapObserver(transportService.sessions);
+    }
+
+    @Bean
+    public MBeanExporter mBeanExporter() {
+        MBeanExporter exporter = new MBeanExporter();
+        Map<String, Object> beans = new HashMap<>();
+        beans.put("org.thingsboard:type=TransportSessionMapObserver", hashMapObserver());
+        exporter.setBeans(beans);
+        return exporter;
+    }
+
+}